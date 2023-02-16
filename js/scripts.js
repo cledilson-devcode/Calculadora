c82a89ea6ccaa48079ae5bde1812515f55dfb56f
Cledilson Filho <cledilsondevoffice@gmail.com>
2023-02-15 22:17:24 -0300

    ajustando operações básicas

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,15 @@ class Calculator {
     // add digit to calculator screen
     addDigit(digit){
         // check if current operation already has a dot
+        
         if (digit === "," && this.currentOperationText.innerText.includes(",")) {
             return;
         }
 
+        // Process all calculator operations
+        
         this.currentOperation = digit;
+        
         this.updateScreen();
     }
 
@@ -29,12 +33,24 @@ class Calculator {
         
         // Get current and previous value
         let operationValue;
-        const previous = parseFloat(this.previousOperationText.innerText);
+        const previous = parseFloat(this.previousOperationText.innerText.split(" ")[0]);
         const current = parseFloat(this.currentOperationText.innerText);
-
+        
         switch (operation) {
             case "+":
                 operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "-":
+                operationValue = previous - current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "/":
+                operationValue = previous / current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "*":
+                operationValue = previous * current;
                 this.updateScreen(operationValue, operation, current, previous);
                 break;
             default:
@@ -42,26 +58,30 @@ class Calculator {
         }
     }
 
-
+    // Change values of the calculator screen
     updateScreen(
         operationValue = null, 
         operation = null, 
         current = null, 
         previous = null){
-
+        // console.log(operationValue, operation, current, previous);
         if (operationValue === null) {
             this.currentOperationText.innerText += this.currentOperation;
+            
         } else {
             // Check if value is zero, if it is just add current value
             if (previous === 0) {
                 operationValue = current;
             }
+            
             // Add current value to previous
-            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.previousOperationText.innerText = `${operationValue} ${operation}`;
 
+            this.currentOperationText.innerText = "";
         }
 
-        this.currentOperationText.innerText += this.currentOperation;
+        // this.currentOperationText.innerText += this.currentOperation;
+        
     }
 
 }
@@ -73,8 +93,10 @@ buttons.forEach((btn) => {
         const value = e.target.innerText;
         
         if (parseFloat(value) >= 0 || value === ",") {
+            
             calc.addDigit(value);
         }else{
+            
             calc.processOperation(value);
         }
 
